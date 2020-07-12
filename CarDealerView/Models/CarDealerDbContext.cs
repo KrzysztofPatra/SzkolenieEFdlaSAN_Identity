ffb5251ad58274e58711cb3e2e850dc86fb09a7e
KrzysztofPatra <krzysztof.patra@gmail.com>
2020-07-12 22:43:50 +0200

    Pierwszy commit

---
+++ CarDealerView/Models/CarDealerDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarDealerView.Models
+{
+    public class CarDealerDbContext: IdentityDbContext<ApplicationUser>
+    {
+
+        public CarDealerDbContext(DbContextOptions<CarDealerDbContext> options)
+       : base(options)
+        { }
+
+
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<CarModel> CarModels { get; set; }
+
+
+    }
+}
